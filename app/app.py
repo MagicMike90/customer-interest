171d1bd0c07a7d3a1e6de79a7e05ec52583ce265
Michael Luo <mluo@echoice.com.au>
2017-12-18 17:00:25 +1100

    update app

--- app/app.py
+++ app/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask import Flask, jsonify
 from sklearn.externals import joblib
 import pandas as pd
-
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
      json_ = request.json
+     print(json_)
      query_df = pd.DataFrame(json_)
      query = pd.get_dummies(query_df)
      for col in model_columns:
