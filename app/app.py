4b16a277a47e96f961ebf0e410d33d8c5e737c05
Michael Luo <mluo@echoice.com.au>
2017-12-19 12:58:03 +1100

    update model

--- app/app.py
+++ app/app.py
@@ -9,20 +9,24 @@ app = Flask(__name__)
 
 def transform(data):
   test = pd.read_json(data, orient='records')
+  print(test)
   query = pd.get_dummies(test)
+  print(query)
   for col in model_columns:
     if col not in query.columns:
       query[col] = 0
+
+  query = query.fillna(0)
+
   return query[model_columns]
 
 def classify(data):
-    label = {0: 'Accpeted', 3: 'Rejected'}
+    label = {1: 'Accpeted', 3: 'Rejected'}
     X = transform(data)
     y = clf.predict(X)
-
     labels = list(map(lambda x: label[x], y))
     proba = list(map(lambda x: np.max(x), clf.predict_proba(X)))
-    
+
     return labels, proba
   
 @app.route('/predict', methods=['POST'])
@@ -30,6 +34,7 @@ def predict():
   try:
     if request.is_json:
       data = json.dumps(request.get_json())
+
       y, proba = classify(data)
       return jsonify({'prediction': list(y),'proba': list(proba)})
   except Exception as inst:
