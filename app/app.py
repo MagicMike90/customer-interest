5c68f3fd9125cff67e3e1cd4f2a2ee398da62dfc
Michael Luo <mluo@echoice.com.au>
2018-01-10 16:33:27 +1100

    update

--- app/app.py
+++ app/app.py
@@ -7,20 +7,30 @@ import numpy as np
 
 app = Flask(__name__)
 
+def changeDateType(df): 
+    df['enquired'] = pd.DatetimeIndex(df['enquired'])
+    df['loan amount'] = df['loan amount'].astype(int)
+    df['post code'] = df['post code'].astype(int)
+    return df
+
+def getDetailDate(df):
+    # remove Year feature since it is not important (show below random forest)
+    # data_set['Year'] = data_set['Enquired'].dt.year
+    df['month'] = df['enquired'].dt.month
+    df['day'] = df['enquired'].dt.day
+    df['hour'] = df['enquired'].dt.hour
+    df['weekday'] = df['enquired'].dt.weekday_name
+
+    # if 'enquired' in df.columns:
+    #   df = df.loc[:,df.columns != 'enquired']
+    return df
+
 def transform(data):
-  test = pd.read_json(data, orient='records')
-  # print("data {}".format(test))
-  query = pd.get_dummies(test)
-  
-  print("columns {}".format(query.columns))
-  for col in model_columns:
-    if col not in query.columns:
-      query[col] = 0
-
-  # query = query.fillna(0)
-
-  # print(query[model_columns])
-  return query[model_columns]
+  data.columns = map(str.lower, data.columns)
+  data = data.applymap(lambda x: x if not '_' in str(x) else x.replace('_',''))
+  changeDateType(data)
+  getDetailDate(data)
+  return data
 
 def classify(data):
     label = {0: 'Accpeted', 1: 'Rejected'}
@@ -29,23 +39,33 @@ def classify(data):
     print("X {}".format(X))
 
     y = clf.predict(X)
-    labels = list(map(lambda x: label[x], y))
-    proba = list(map(lambda x: np.max(x), clf.predict_proba(X)))
+
+    print("y {}".format(y))
+
+    # labels = list(map(lambda x: label[x], y))
+    # proba = list(map(lambda x: np.max(x), clf.predict_proba(X)))
 
     return labels, proba
   
 @app.route('/predict', methods=['POST'])
 def predict():
-  try:
-    if request.is_json:
-      data = json.dumps(request.get_json())
-
-      y, proba = classify(data)
-      return jsonify({'prediction': list(y),'proba': list(proba)})
-  except Exception as inst:
-    return jsonify({'error': inst})
+  if request.is_json:
+    data = pd.DataFrame(request.get_json(),index=[0])
+    data = transform(data)
+    y, proba = classify(data)
+    return jsonify('worked!')
+  # try:
+  #   if request.is_json:
+  #     # print(request.get_json())
+  #     data = pd.DataFrame(request.get_json(),index=[0])
+  #     # data = json.dumps(request.get_json())
+  #     y, proba = classify(data)
+  #     return jsonify({'prediction': list(y),'proba': list(proba)})
+  # except Exception as inst:
+  #   return jsonify({'error': inst})
 
 
 if __name__ == '__main__':
      clf = joblib.load('models/lrpipeline.pkl')
      model_columns = joblib.load('models/model_columns.pkl')
+     app.run(debug=True)
