2ceecd0ff06325499ce9556f12c3505bb2666c4a
Michael Luo <mluo@echoice.com.au>
2018-01-11 16:15:54 +1100

    update

--- app/app.py
+++ app/app.py
@@ -3,49 +3,41 @@ import json
 from sklearn.externals import joblib
 import pandas as pd
 import numpy as np
-
+from labelEncoder import MultiColumnLabelEncoder
 
 app = Flask(__name__)
 
 def changeDateType(df): 
+  if 'enquired' in df.columns:
     df['enquired'] = pd.DatetimeIndex(df['enquired'])
-    df['loan amount'] = df['loan amount'].astype(int)
-    df['post code'] = df['post code'].astype(int)
-    return df
+  df['loan amount'] = df['loan amount'].astype(int)
+  df['post code'] = df['post code'].astype(int)
+  return df
 
 def getDetailDate(df):
     # remove Year feature since it is not important (show below random forest)
     # data_set['Year'] = data_set['Enquired'].dt.year
-    df['month'] = df['enquired'].dt.month
-    df['day'] = df['enquired'].dt.day
-    df['hour'] = df['enquired'].dt.hour
-    df['weekday'] = df['enquired'].dt.weekday_name
-
-    # if 'enquired' in df.columns:
-    #   df = df.loc[:,df.columns != 'enquired']
+    if 'enquired' in df.columns:
+      df['month'] = df['enquired'].dt.month
+      df['day'] = df['enquired'].dt.day
+      df['hour'] = df['enquired'].dt.hour
+      df['weekday'] = df['enquired'].dt.weekday_name
+      df = df.loc[:,df.columns != 'enquired']
     return df
 
 def transform(data):
   data.columns = map(str.lower, data.columns)
-  data = data.applymap(lambda x: x if not '_' in str(x) else x.replace('_',''))
+  data.columns = data.columns.str.replace('_', ' ')
   changeDateType(data)
-  getDetailDate(data)
+  data = getDetailDate(data)
   return data
 
 def classify(data):
-    label = {0: 'Accpeted', 1: 'Rejected'}
     X = transform(data)
-
-    print("X {}".format(X))
-
-    y = clf.predict(X)
-
-    print("y {}".format(y))
-
-    # labels = list(map(lambda x: label[x], y))
-    # proba = list(map(lambda x: np.max(x), clf.predict_proba(X)))
-
-    return labels, proba
+    print(X)
+    y = clf.predict(X)[0]
+    proba = np.max(clf.predict_proba(X))
+    return y, proba
   
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -53,7 +45,11 @@ def predict():
     data = pd.DataFrame(request.get_json(),index=[0])
     data = transform(data)
     y, proba = classify(data)
-    return jsonify('worked!')
+
+    print(y)
+    print(proba)
+
+    return jsonify({'prediction': 'b','proba': "a"})
   # try:
   #   if request.is_json:
   #     # print(request.get_json())
